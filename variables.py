3bee33e43ba8c0dc68154451b156486dd13830aa
kadrian96 <76454999+kadrian96@users.noreply.github.com>
2021-01-12 23:21:35 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 14:18:24 2021
+
+@author: wkevi
+"""
+
+print(type(5))
+print(type(8.5))
+print(type('Python'))
+print(type(False))
+print(8>5)
+print(8>=5)
+print(8==5)
+print(8<5)
+print(8<=5)
+x=5
+print(x,'\n'*2) #salto de linea
+print(x**5,'\n'*2)
+print(x/5,'\n'*2)
+print(x-5,'\n'*2)
+print(x+5,'\n'*2)
+print(x%5,'\n'*2)
+print(x//2,'\n'*2)
